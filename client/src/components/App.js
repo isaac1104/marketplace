62e6e988e5531c285159f1b356845d8b6cb41194
isaac1104 <isaac-kwon@hotmail.com>
2018-01-20 11:02:57 -0800

    init

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Responsive } from "semantic-ui-react";
+
+const App = () => {
+  return (
+    <Responsive>
+      <h1>Welcome to the MARKETPLACE</h1>
+    </Responsive>
+  );
+}
+
+export default App;

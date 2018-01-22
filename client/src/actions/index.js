2ff3ec983fa754e95c5c67806ccc82d8d2cbcd50
isaac1104 <isaac-kwon@hotmail.com>
2018-01-21 22:31:42 -0800

    Building for production

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import { FETCH_DATA, POST_NEW_USER } from "./types";
 
 export const fetchData = () => async dispatch => {
-  const request = await axios.get("http://54.215.120.93/functions.php?type=userdata");
+  const request = await axios.get("https://54.215.120.93/api/users/userdata.php");
   const { data } = request;
   dispatch({ type: FETCH_DATA, payload: data });
 }
 
 export const postNewUser = () => async dispatch => {
-  const request = await axios.post("http://54.215.120.93/functions.php?type=add-user", {
+  const request = await axios.post("https://54.215.120.93/api/users/add.php", {
         username: "isaac1104",
         password: "isaac",
         email: "isaac-kwon@hotmail.com",

5f7ae2147c5741f79273bb96f022a4955ae0da21
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-01-25 15:39:33 -0800

    search page added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ import AddPost from "./../pages/AddPost";
 import MyProfile from "./../pages/MyProfile";
 import MyPost from "./../pages/MyPost";
 import MyComment from "./../pages/MyComment";
+import Search from "./../pages/Search";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
           <Route exact path="/my-profile" component={MyProfile}/>
           <Route exact path="/my-posts" component={MyPost} />
           <Route exact path="/my-comments" component={MyComment} />
+          <Route exact path="/search/:query" component={Search} />
         </Switch>
       </div>
     </Router>

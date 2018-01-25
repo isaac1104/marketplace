5ec1ed15f91a1aad45a077c7df8d9217440f771f
isaac1104 <isaac-kwon@hotmail.com>
2018-01-24 17:19:16 -0800

    conditional rendering on AddPostContent & CommentField

--- client/src/containers/AddPostContent.js
+++ client/src/containers/AddPostContent.js
@@ -1,20 +1,49 @@
 import React, { Component } from "react";
 import PostForm from "./PostForm";
+import { Dimmer, Header } from "semantic-ui-react";
+import { connect } from "react-redux";
+import * as actions from "./../actions";
 
-export default class PostContent extends Component {
+class PostContent extends Component {
+
+  renderPostForm = () => {
+    if (this.props.user.loggedIn) {
+      return (
+        <div className="container">
+          <h1 className="display-4">New Post</h1>
+          <p className="lead">Some post policy/regulations can go here.</p>
+          <hr className="my-2" />
+          <PostForm />
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <Dimmer active>
+            <Header as="h2" style={{ color: "white" }}>
+              Please sign in to create a post
+            </Header>
+          </Dimmer>
+        </div>
+      );
+    }
+  }
 
   render () {
     return (
       <div>
         <div className="jumbotron jumbotron-fluid py-2">
-          <div className="container">
-            <h1 className="display-4">New Post</h1>
-            <p className="lead">Some post policy/regulations can go here.</p>
-            <hr className="my-2" />
-            <PostForm />
-          </div>
+          {this.renderPostForm()}
         </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    user: state.logInUser
+  }
+}
+
+export default connect(mapStateToProps, actions)(PostContent);

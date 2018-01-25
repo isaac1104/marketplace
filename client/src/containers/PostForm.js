35b52b657c03cc0a4e27bb59c6b6bad4ed7118f9
isaac1104 <isaac-kwon@hotmail.com>
2018-01-24 21:20:42 -0800

    able to add new post

--- client/src/containers/PostForm.js
+++ client/src/containers/PostForm.js
@@ -2,66 +2,93 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import * as actions from './../actions';
 import { connect } from 'react-redux';
-import { Input } from "semantic-ui-react";
+import { Input, Button } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
 
 class PostForm extends Component {
 
+  componentWillUnmount() {
+    this.props.resetPostState();
+  }
+
   formSubmit = () => {
     const post = this.props.form.post.values;
-    console.log(post);
+    this.props.addNewPost(this.props.user.data[0].username, post.title, post.category, post.location, post.price, post.description);
+  }
+
+  redirectToMarket = () => {
+    if (this.props.newPost.success) {
+      return (
+        <Redirect to={"/market"}/>
+      );
+    } else {
+      return (
+        <div></div>
+      );
+    }
   }
 
   render () {
 
-    const { handleSubmit } = this.props;
+    const { handleSubmit, pristine, submitting } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.formSubmit)}>
-        <div>
-          <div className="form-group">
-            <label>Title</label>
-            <Field name="title" component="input" type="text" className="form-control" style={{maxWidth:"380px"}} autoComplete="off"/>
+      <div>
+        <form onSubmit={handleSubmit(this.formSubmit)}>
+          <div>
+            <div className="form-group">
+              <label>Title</label>
+              <Field name="title" component="input" type="text" className="form-control" style={{maxWidth:"380px"}} autoComplete="off"/>
+            </div>
+            <div className="form-group">
+              <label>Category</label>
+              <Field name="category" component="select" className="form-control" style={{maxWidth:"380px"}}>
+                <option value="">Select a Category</option>
+                <option value="furniture">Furniture</option>
+                <option value="electronics">Electronics</option>
+                <option value="health&beauty">Health & Beauty</option>
+                <option value="books">Books</option>
+                <option value="officesupplies">Office Supplies</option>
+                <option value="automotive">Automotive</option>
+              </Field>
+            </div>
+            <div className="form-group w-75">
+              <label>Location</label>
+              <Field name="location" component="input" type="text" className="form-control" style={{maxWidth:"380px"}} autoComplete="off"/>
+            </div>
+            <div className="form-group w-75">
+              <label className="d-block">Price</label>
+              <Field
+                name="price"
+                component={ Input }
+                icon="dollar"
+                iconPosition="left"
+                size="small"
+                type="number"
+                autoComplete="off"
+                fluid
+                style={{maxWidth:"380px"}}
+              />
+            </div>
+            <div className="form-group">
+              <label>Description</label>
+              <Field name="description" component="textarea" type="text" className="form-control" style={{resize:"none", height: "160px"}}/>
+            </div>
+            <div className="text-center">
+              <Button
+                className="btn btn-outline-primary mr-5"
+                type="submit"
+                disabled={pristine || submitting}
+                color="blue"
+              >
+                Upload
+              </Button>
+            </div>
           </div>
-          <div className="form-group">
-            <label>Category</label>
-            <select className="form-control" style={{maxWidth:"380px"}}>
-              <option value="">Select a Category</option>
-              <option value="furniture">Furniture</option>
-              <option value="electronics">Electronics</option>
-              <option value="health&beauty">Health & Beauty</option>
-              <option value="books">Books</option>
-              <option value="officesupplies">Office Supplies</option>
-              <option value="automotive">Automotive</option>
-            </select>
-          </div>
-          <div className="form-group w-75">
-            <label>Location</label>
-            <Field name="location" component="input" type="text" className="form-control" style={{maxWidth:"380px"}} autoComplete="off"/>
-          </div>
-          <div className="form-group w-75">
-            <label className="d-block">Price</label>
-            <Field
-              name="price"
-              component={ Input }
-              icon="dollar"
-              iconPosition="left"
-              size="small"
-              type="number"
-              autoComplete="off"
-              fluid
-              style={{maxWidth:"380px"}}
-            />
-          </div>
-          <div className="form-group">
-            <label>Description</label>
-            <Field name="description" component="textarea" type="text" className="form-control" style={{resize:"none", height: "160px"}}/>
-          </div>
-          <div className="text-center">
-            <button className="btn btn-outline-primary mr-5">Upload</button>
-            <button className="btn btn-outline-danger" onClick={()=>{window.history.back()}}>Cancel</button>
-          </div>
-        </div>
-      </form>
+          {this.redirectToMarket()}
+        </form>
+        <Button color="red" onClick={()=>{window.history.back()}}>Cancel</Button>
+      </div>
     );
   }
 }
@@ -69,6 +96,8 @@ class PostForm extends Component {
 function mapStateToProps(state) {
   return {
     form: state.form,
+    newPost: state.newPost,
+    user: state.logInUser
   };
 }
 
--- client/src/reducers/add_new_post_reducer.js
+++ client/src/reducers/add_new_post_reducer.js
@@ -1,10 +1,12 @@
-import { ADD_NEW_POST } from "./../actions/types";
+import { ADD_NEW_POST, RESET_POST_STATE } from "./../actions/types";
 
 const initialState = {};
 
 function addNewPostReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_NEW_POST:
+      return action.payload;
+    case RESET_POST_STATE:
       return action.payload;
     default:
       return state;

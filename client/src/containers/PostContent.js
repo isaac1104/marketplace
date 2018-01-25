440fabf580fd910310f32db7d982194cab484e8c
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-01-25 14:29:56 -0800

    delete,edit post & delete user btn added

--- client/src/containers/PostContent.js
+++ client/src/containers/PostContent.js
@@ -18,17 +18,45 @@ class PostContent extends Component {
     if (this.props.postData) {
       const data = this.props.postData[0];
       let date = new Date(data.timestamp+' UTC');
-      let description = data.description.toString();
       return (
         <div>
-          <button className="btn btn-outline-info rounded-circle" onClick={() => {this.goBack()}}><i className="fa fa-arrow-left" aria-hidden="true"></i></button>
+          <div className="row justify-content-between">
+            <button className="btn btn-outline-info rounded-circle" onClick={() => {this.goBack()}}><i className="fa fa-arrow-left" aria-hidden="true"></i></button>
+            <div>
+              <button className="btn btn-outline-success mr-4" type="button">Edit Post</button>
+              <button className="btn btn-outline-danger" type="button" data-toggle="modal" data-target="#delete-confirmation">Delete Post</button>
+
+              <div className="modal fade" id="delete-confirmation" tabIndex="-1" role="dialog">
+                <div className="modal-dialog" role="document">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title text-danger"><i className="fa fa-exclamation-triangle" aria-hidden="true"></i> Delete Post</h5>
+                      <button type="button" className="close" data-dismiss="modal">
+                        <span>&times;</span>
+                      </button>
+                    </div>
+                    <div className="modal-body text-center">
+                      <h3>Are You Sure?</h3>
+                      <p>This will delete all comments belong to this post as well.</p>
+                      <p className="text-danger">This action is irreversible.</p>
+                    </div>
+                    <div className="modal-footer">
+                      <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                      <button type="button" className="btn btn-danger">Delete Post</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
           <h1>{data.title}</h1>
           <h4>{data.author}<span className="badge badge-pill badge-secondary ml-3"><i className="fa fa-clock-o" aria-hidden="true"></i> {date.toLocaleString()}</span></h4>
           <hr/>
           <img className="card-img img-fluid" src="http://via.placeholder.com/350x200" alt=""/>
           <hr/>
           <div className="card-body">
-            <p className="card-text" style={{whiteSpace:"pre"}}>{description}</p>
+            <p className="card-text" style={{whiteSpace:"pre"}}>{data.description}</p>
             <p className="card-text">{data.location}</p>
             <p className="card-text">$ {data.price}</p>
           </div>
